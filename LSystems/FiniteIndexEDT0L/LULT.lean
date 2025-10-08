f7d20056eafe1f3dd427c98020aea3182b06c45e
Alex Bishop <alexbishop1234@gmail.com>
2025-10-08 13:42:09 +0200

    Fixes the definition of LULT grammars
    The previous definition of LULT grammars used an incorrect quantifier.
    This update also simplifies the definitions and removes the requirement
    that `BEq (Symbol T N)` can be instanciated.

--- LSystems/FiniteIndexEDT0L/LULT.lean
+++ LSystems/FiniteIndexEDT0L/LULT.lean
@@ -7,18 +7,20 @@ import LSystems.EDT0L.Defs
 import LSystems.EDT0L.DeriveSequence
 
 namespace EDT0LGrammar
-variable {T N H : Type*} [Fintype N] [Fintype H]
-variable (E : EDT0LGrammar T N H)
-variable [BEq (Symbol T N)]
 
-def IsLULT :=
+def ContainedAtMostOnce {α : Type*} (a : α) (s : List α) : Prop :=
+  a ∉ s ∨ ∃ s₁ s₂ : List α, a ∉ s₁ ∧ a ∉ s₂ ∧ s = s₁ ++ [a] ++ s₂
+
+def IsLULT {T N H : Type*} [Fintype N] [Fintype H] (E : EDT0LGrammar T N H) : Prop :=
   ∀ w ∈ E.language, ∃ a : List H,
   ∃ _ : E.DeriveSequence a [.nonterminal E.initial] = w.map .terminal,
-    ∀ n : N, ∀ a₁ a₂ : List H, ∃ _ : a = a₁ ++ a₂,
-      (List.count
-        (.nonterminal n)
-        (E.DeriveSequence a₁ [.nonterminal E.initial]) ≤ 1) ∨ 
-      (List.length
-        (E.DeriveSequence a₂ [.nonterminal n]) ≤ 1)
+    ∀ n : N, ∀ a₁ a₂ : List H, a = a₁ ++ a₂ →
+        let before_split := E.DeriveSequence a₁ [.nonterminal E.initial];
+        let after_split := E.DeriveSequence a₂ [.nonterminal n];
+        (ContainedAtMostOnce (.nonterminal n) before_split) ∨ (List.length after_split ≤ 1)
 
 end EDT0LGrammar
+
+def Language.IsLULT {α : Type*} (L : Language α) : Prop :=
+  ∃ n m : ℕ, ∃ E : EDT0LGrammar α (Fin n) (Fin m), ∃ _ : E.IsLULT, E.language = L
+
